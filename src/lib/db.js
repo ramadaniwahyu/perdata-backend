c030922b2de9951f15d76ff3ec34dabb7f8bcc9b
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-11 11:16:06 +0700

    isActive false

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Database connected ${conn.connection.host}`);
+        console.log(`Database connected ${conn.connection.host} on ${conn.connection.name}`);
         // console.log('Database connected')
     
     } catch (error) {
