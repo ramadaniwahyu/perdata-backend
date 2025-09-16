9e96cedeb961a120b559ee796e9bb638cafc4a80
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-09-16 14:11:08 +0700

    relaas 25

--- src/controllers/panggilanCtrl.js
+++ src/controllers/panggilanCtrl.js
@@ -37,7 +37,7 @@ class APIfeatures {
 
     paginating(){
         const page = this.queryString.page * 1 || 1
-        const limit = this.queryString.limit * 1 || 10
+        const limit = this.queryString.limit * 1 || 25
         const skip = (page - 1) * limit;
         this.query = this.query.skip(skip).limit(limit)
         return this;
